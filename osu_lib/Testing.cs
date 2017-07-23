f6ba81b0d5b7e12f836a9b0d15f4b0a0038131a8
NotMichaelChen <mzc12345@gmail.com>
2017-07-23 16:33:20 -0400

    Removed HitObjectListParser, filled in the testing class

--- osu_lib/Testing.cs
+++ osu_lib/Testing.cs
@@ -1,6 +1,28 @@
-﻿public class Testing
+﻿using System;
+using System.Diagnostics;
+
+using osu_lib;
+
+public class Testing
 {
     public static void Main(string[] args)
     {
+        if(args.Length == 0)
+        {
+            Console.WriteLine("Please enter beatmap filename (or drag onto application)");
+            return;
+        }
+
+        Stopwatch timer = new Stopwatch();
+
+        foreach(string name in args)
+        {
+            timer.Start();
+            HitPointParser parser = new HitPointParser(name);
+            timer.Stop();
+
+            Console.WriteLine(parser.GetMetadata("title") + ": " + parser.GetMetadata("version"));
+            Console.WriteLine("Parsed in " + timer.ElapsedMilliseconds + " ms");
+        }
     }
 }
